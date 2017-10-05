f883d3b3a5fb7af6e78fce671ece404ed31c8915
Mariana Carmona <mariana@Felipes-iMac.local>
2017-10-05 16:14:56 -0500

    primer commit

---
+++ src/codigo_inicial.R
@@ -0,0 +1,19 @@
+library(readr)
+
+df_t <- read_csv('train.csv')
+test <- read_csv('test.csv')
+table(df_t$estado)
+
+
+
+mostrar_imagen <- function(renglon, dat){
+  v <- as.numeric(dat %>% dplyr::select(contains('X'))%>% .[renglon,])
+  mat <- (t(matrix(v, nrow=190, ncol=200, byrow=T))[,190:1])
+  image(mat, axes = F, col=gray(0:255/255))
+}
+
+mostrar_imagen(1, df_t)
+mostrar_imagen(100, df_t)
+mostrar_imagen(190, df_t)
+mostrar_imagen(130, df_t)
+
